0c3f959c464c9afad1a7fcfe6cabe6ce1f86885d
DreaTheEmpress <benc0007@videndjurs.net>
2026-04-29 13:56:34 +0200

    nav bar!

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,15 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
     return (
-        <nav className="relative grid w-full h-auto bg-black justify-center">
-            <ul className="grid grid-flow-col m-4 p-0 list-none text-white">
-                <li className="pl-10 pr-10 text-2xl font-bold uppercase">events</li>
-                <li className="pl-10 pr-10 text-2xl font-bold uppercase">Bliv Medlem</li>
-                <li className="pl-10 pr-10 text-2xl font-bold uppercase">Om Os</li>
-                <li className="pl-10 pr-10 text-2xl font-bold uppercase">Kontakt</li>
-                <li className="pl-10 pr-10 text-2xl font-bold uppercase">Info</li>
+        <nav className="relative grid grid-flow-col w-full h-auto bg-black justify-center items-center">
+            <Image
+            src={"/logo-1.png"}
+            alt="Picture of the author"
+            width={200}
+            height={200}
+            className="block p-2"
+            loading="eager"
+            />
+            <ul className="grid grid-flow-col m-4 p-3 list-none text-white">
+                <li className="pl-10 pr-10 text-2xl uppercase">events</li>
+                <li className="pl-10 pr-10 text-2xl uppercase">Bliv Medlem</li>
+                <li className="pl-10 pr-10 text-2xl uppercase">Om Os</li>
+                <li className="pl-10 pr-10 text-2xl uppercase">Kontakt</li>
+                <li className="pl-10 pr-10 text-2xl uppercase">Info</li>
             </ul>
+            <button className="button-bg butt-text-col p-3 rounded-lg button-shadow">
+                Køb Billeter
+            </button>
         </nav>
     )
 }
